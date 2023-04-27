475fda015fed2882d242e59941a4c6943022bf19
adroy2f <113098540+adroy2f@users.noreply.github.com>
2023-04-27 17:07:29 +0000

    created controller files without content

---
+++ controllers/availsController.js
@@ -0,0 +1,15 @@
+//importing modules
+// const bcrypt = require("bcrypt");
+const db = require("../models");
+// const jwt = require("jsonwebtoken");
+
+const avails = db.avails;
+
+
+
+
+
+
+module.exports = {
+    
+};
---
+++ controllers/hostelResidentController.js
@@ -0,0 +1,15 @@
+//importing modules
+// const bcrypt = require("bcrypt");
+const db = require("../models");
+// const jwt = require("jsonwebtoken");
+
+const hostelResident = db.hostelResident;
+
+
+
+
+
+
+module.exports = {
+    
+};
