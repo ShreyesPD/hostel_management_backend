63ea44f2377e44db27a8bd57db27ceed79e0daa4
shreyes-pd1 <mca.2204@unigoa.ac.in>
2023-04-28 03:16:01 +0530

    added getAccomodationFee

--- routes/hostelRoutes.js
+++ routes/hostelRoutes.js
@@ -1,13 +1,14 @@
 //importing modules
 const express = require('express')
 const hostelController = require('../controllers/hostelController')
-const {  createHostel , getVacancy } = hostelController
+const {  createHostel , getVacancy , getAccomodationFee} = hostelController
 // const userAuth = require('../Middlewares/userAuth')
 
 const router = express.Router()
 
 router.post('/createHostel/id/:hostel_id/capacity/:capacity/h_type/:hostel_type/fee/:accommodation_fee_per_day/deposit/:security_deposit/location/:location/cont/:office_contact/email/:office_email/descr/:hostel_descript', createHostel)
 router.get('/getVacancy/:hostel_type/', getVacancy)
+router.get('/getAccomodationFee/:hostel_type/', getAccomodationFee)
 
 
 module.exports = router
