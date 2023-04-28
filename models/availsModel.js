2639116c6c69f30701d025ff3a6c7ac0da9a326f
adroy2f <113098540+adroy2f@users.noreply.github.com>
2023-04-28 06:38:55 +0000

    updated avails model

--- models/availsModel.js
+++ models/availsModel.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             primaryKey: true,
             references: {
-                model: 'messes',
+                model: 'mess_package',
                 key: 'package_id'
             },
             type: DataTypes.STRING,
