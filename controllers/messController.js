0e50112a4dd11f8eadab3d266d6ccbfac4f86b33
shreyes-pd1 <mca.2204@unigoa.ac.in>
2023-04-28 12:36:41 +0530

    new file

--- controllers/messController.js
+++ controllers/messController.js
@@ -7,24 +7,24 @@ const mess = db.mess;
 const hostelResidents = db.hostelResident
 
 
-// const getMessFeePerday = async(req,res) => {
-//     console.log('hello')
-//     try{
+const getMessFeePerday = async(req,res) => {
+    console.log('hello')
+    try{
 
-//         const host_type = req.params['hostel_type']
-//         const hostFee = await hostels.findOne({
-//             attributes :['accommodation_fee_per_day'] ,
-//             where : {
-//                 hostel_type : host_type
-//             }
-//         })
+        const host_type = req.params['hostel_type']
+        const hostFee = await hostels.findOne({
+            attributes :['accommodation_fee_per_day'] ,
+            where : {
+                hostel_type : host_type
+            }
+        })
     
-//         console.log({hostFee});
-//         res.status(200).send(hostFee);
-//     }catch (error){
-//         console.log(error);
-//     }
-// };
+        console.log({hostFee});
+        res.status(200).send(hostFee);
+    }catch (error){
+        console.log(error);
+    }
+};
 
 
 
